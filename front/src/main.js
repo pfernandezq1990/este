fcd377c9ec9e53112be9e4a35d9a090addf1ffe2
pfernandezq1990 <pfernandezq1990@gmail.com>
2021-03-13 10:48:20 -0500

    i18n plugin

--- front/src/main.js
+++ front/src/main.js
@@ -8,6 +8,7 @@ import vuetify from './plugins/vuetify';
 import date from './filters/date';
 import datetime from './filters/datetime'
 import DatetimePicker from 'vuetify-datetime-picker'
+import i18n from './i18n'
 
 
 Vue.use(DatetimePicker)
@@ -22,5 +23,6 @@ new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
