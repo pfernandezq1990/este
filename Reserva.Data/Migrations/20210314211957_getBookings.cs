38025d2115a781c2d1c446b606228379b605e447
pfernandezq1990 <pfernandezq1990@gmail.com>
2021-03-14 17:24:03 -0400

    SP getBookings for a contact

---
+++ Reserva.Data/Migrations/20210314211957_getBookings.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Reserva.Data.Migrations
+{
+    public partial class getBookings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE OR REPLACE FUNCTION public.getbookig(contact_id integer)
+            RETURNS TABLE(titulo character varying, fecha timestamp without time zone)
+            LANGUAGE plpgsql
+            AS $function$
+                BEGIN
+                    return QUERY select ""Title"", ""Date"" from ""Bookings"" b where ""ContactId"" = contact_id;
+                END;
+                $function$
+            ;";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
